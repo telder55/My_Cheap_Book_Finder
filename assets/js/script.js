84097b08b6ae07ce6d59e40fa128c2c1e40f08f5
TJ Elder <telder55@gmail.com>
2021-02-06 13:55:50 -0700

    fixed pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 // Pseudo Coding for My Cheap Book Finder
 
+//Global Variables Needed
+// Create global variable for DIV with ID "book-info-area" use this variable to create and append elements to this section. 
+
+
 // Searchbox section
 // Create a single searchbox that the user can enter book title, author, or category into. 
 // fetch google books url and return first 5 book titles, authors, and store 5 IDs associated with each result in an array. 
 // Display 5 link options underneath search box and make so when result 1 is chosen, index 0 from IDs array is used as query parameter in second api call to retrieve google book info. Result 2 is index 1 from IDs array etc...
 
 
-// Need a variable to store search keyword. 
-// Create global variable for DIV with ID "book-info-area" use this variable to create and append elements to this section. 
-//Google Book Api Responds with following Items; Book Title, Author, Book Cover Image, Description, retail price, average rating. 
+
+//Google Book Api Responds with following Items; Book Title, Author, Book Cover Image, Description, retail price, average rating, link to buy book new. 
 
 // Have a button to find book on ebay. Use event listener on button to make an api call using book title and author parameters from google books api response. 
 
